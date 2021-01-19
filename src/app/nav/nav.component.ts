882ec7c236d61968d6e6fa39e86dbde09e6de96c
jesshill <jess.hillert@gmail.com>
2021-01-19 16:57:40 +0100

    Locally save lang, trigger amount change on lang and smaller UX fixes

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -10,16 +11,19 @@ export class NavComponent {
   @Input() dark_mode: boolean = false;
   @Output() toogle_dark_mode = new EventEmitter<boolean>();
   flag!: string;
-  current_path: any;
+  current_main_path: any; /// recipe:id only update this if refreshing/directly going to the page
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService, private router: Router) {
     translate.addLangs(['en', 'sv']);
     translate.setDefaultLang('en');
-    const browser_lang = translate.getBrowserLang();
+    const local_storage_lang = localStorage.getItem('lang');
+    const browser_lang = local_storage_lang
+      ? local_storage_lang
+      : translate.getBrowserLang();
     translate.use(browser_lang.match(/en|sv/) ? browser_lang : 'en');
     this.setFlag(browser_lang);
 
-    this.current_path = window.location.pathname;
+    this.current_main_path = window.location.pathname;
   }
 
   setFlag(browser_lang: string) {
@@ -36,6 +40,15 @@ export class NavComponent {
   selectLang(value: string) {
     this.translate.use(value);
     this.setFlag(value);
+    localStorage.setItem('lang', value);
+
+    /** To trigger recipe amout change */
+    const current_exact_path = window.location.pathname;
+    if (current_exact_path.slice(0, 7) === '/recipe') {
+      this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+        this.router.navigateByUrl(current_exact_path);
+      });
+    }
   }
 
   toggleDarkMode() {
